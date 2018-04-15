02616015f111992eaa1dca8310963f06b13f891a
Parimal Yeole <parimal.yeole1@gmail.com>
2018-04-15 15:04:45 +0530

    added boom and error handling file added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const Hapi = require("hapi");
 const Path = require("path");
 const uuid = require("uuid");
 const fs = require("fs");
+const Joi = require("joi");
+const Boom = require("boom");
 
 const cards = loadCards();
 
@@ -13,7 +15,17 @@ const server = Hapi.server({
 });
 
 server.ext("onRequest", (request, h) => {
-  console.log(`Request url on hapi server :: ${request.path}`);
+  console.log(`ext onRequest handler running ...
+  Request url on hapi server :: ${request.path}
+  `);
+  return h.continue;
+});
+
+server.ext("onPreResponse", (request, h) => {
+  console.log(`ext onPreResponse handler running ...`);
+  if(request.response.isBoom){
+    return h.view("error",request.response);
+  }
   return h.continue;
 });
 
@@ -69,22 +81,34 @@ const ServerInit = async () => {
     }
   });
 
+  const cardSchema = Joi.object().keys({
+    name: Joi.string().min(3).max(50).required(),
+    recipient_email: Joi.string().email().required(),
+    sender_name: Joi.string().min(3).max(50).required(),
+    sender_email: Joi.string().email().required(),
+    card_image: Joi.string().regex(/.+\.(jpg|bmp|png|gif)\b/).required()
+  });
+
 
   function newCardHandler(request, h) {
     if (request.method === "get") {
       //return h.file("templates/new.html");
-      return h.view("new");
+      return h.view("new", { card_images: loadImages() });
     } else {
-      const card = {
-        name: request.payload.name,
-        recipient_email: request.payload.recipient_email,
-        sender_name: request.payload.sender_name,
-        sender_email: request.payload.sender_email,
-        card_image: request.payload.card_image
-      }
-      saveCards(card);
-      console.log(card);
-      return h.redirect("/cards");
+      return Joi.validate(request.payload, cardSchema, function (err, val) {
+        if (err) {
+          return Boom.badRequest(err.details[0].message)
+        }
+        var card = {
+          name: val.name,
+          recipient_email: val.recipient_email,
+          sender_name: val.sender_name,
+          sender_email: val.sender_email,
+          card_image: val.card_image
+        };
+        saveCard(card);
+        return h.redirect('/cards');
+      });
     }
   }
 
@@ -92,7 +116,7 @@ const ServerInit = async () => {
     return h.view("cards", { cards: cards });
   }
 
-  function saveCards(card) {
+  function saveCard(card) {
     const id = uuid.v1();
     card.id = id;
     cards[id] = card;
@@ -112,6 +136,10 @@ process.on("unhandledRejection", err => {
 function loadCards() {
   const file = fs.readFileSync('./cards.json');
   return JSON.parse(file.toString());
+}
+
+function loadImages() {
+  return fs.readdirSync("./public/images/cards")
 }
 
 ServerInit();
